5ab68e84d4e651f94ead37cb177ae10f3e8dfbc2
divyadontula <127486095+divyadontula@users.noreply.github.com>
2023-03-10 15:59:56 +0530

    Add files via upload

---
+++ project.java
@@ -0,0 +1,8 @@
+package com.testingautomation;
+
+public class project
+{
+    public static void main(String args[]) {
+        System.out.println("welcome to testing automation classes");
+    }
+}
