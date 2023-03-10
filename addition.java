5fa4d35a4732725dc16aa6c04ed1ac0779d4d494
divyadontula <127486095+divyadontula@users.noreply.github.com>
2023-03-10 16:11:45 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static void main(String args[])
+    {
+        int d = 10;
+        int s = 20;
+        int sum = d + s;
+        System.out.println(d + " + " + s + " = "  + sum);
+    }
+}
